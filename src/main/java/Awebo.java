f8dfc9cc0cce95d4b7fa0e46da55bd93247d04df
superidol2 <115440086+superidol2@users.noreply.github.com>
2025-02-03 00:56:48 +0800

    Add Awebo.java

---
+++ src/main/java/Awebo.java
@@ -0,0 +1,4 @@
+public class Awebo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Awebo\nWhat can I do for you?\nGoodbye! aWebO");
+        System.exit(0);}}
