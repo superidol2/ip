64b10871b6423ca2f101fa0a141a4bd8346089c2
superidol2 <115440086+superidol2@users.noreply.github.com>
2025-02-03 01:12:16 +0800

    Add echo to Awebo.java

--- src/main/java/Awebo.java
+++ src/main/java/Awebo.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 public class Awebo {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Awebo\nWhat can I do for you?\nGoodbye! aWebO");
-        System.exit(0);}}
+        Scanner scanner = new Scanner(System.in); //get user input
+        System.out.println("Hello! I'm Awebo\nWhat can I do for you?\n");
+        while (true) {
+            String userinput = scanner.nextLine();
+            if (userinput.equalsIgnoreCase("bye")) { //case-insensitive check
+                System.out.println("Goodbye! aWebO");
+                        System.exit(0);
+            } else {
+                System.out.println(userinput); //echo
+            }
+        }
+    }
+}
