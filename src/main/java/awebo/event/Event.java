77290d22c4d6e6ae707c86dff3bc449c28ae94a7
superidol2 <115440086+superidol2@users.noreply.github.com>
2025-02-10 16:19:58 +0800

    Change public variables to private with get functions

--- src/main/java/awebo/event/Event.java
+++ src/main/java/awebo/event/Event.java
@@ -3,14 +3,20 @@ package awebo.event;
 import awebo.task.Task;
 
 public class Event extends Task {
-    String from, to;
+    private String from;
+    private String to;
 
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
-
+    public String getFrom() {
+        return from;
+    }
+    public String getTo() {
+        return to;
+    }
     @Override
     public String getType() {
         return "E"; // "E" for Awebo.Event.Awebo.Event
