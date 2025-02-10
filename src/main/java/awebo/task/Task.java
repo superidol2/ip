d33eeb533986a9ce35d4f863384c725f1c3e3c68
superidol2 <115440086+superidol2@users.noreply.github.com>
2025-02-10 16:20:41 +0800

    Change public variables to private with get functions

--- src/main/java/awebo/task/Task.java
+++ src/main/java/awebo/task/Task.java
@@ -1,12 +1,20 @@
 package awebo.task;
 
 public class Task {
-    String description;
-    boolean isDone;
+    private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
     }
 
     public void markDone() {
