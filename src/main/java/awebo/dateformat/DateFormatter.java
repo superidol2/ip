437ff1b815499250447b178a744064523401e44a
superidol2 <115440086+superidol2@users.noreply.github.com>
2025-02-10 16:18:41 +0800

    Change format of switch case

--- src/main/java/awebo/dateformat/DateFormatter.java
+++ src/main/java/awebo/dateformat/DateFormatter.java
@@ -28,14 +28,14 @@ public abstract class DateFormatter {
             return "th"; //special case for 11th, 12th, 13th
         } else {
             switch (day % 10) {
-                case 1:
-                    return "st";
-                case 2:
-                    return "nd";
-                case 3:
-                    return "rd";
-                default:
-                    return "th";
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
             }
         }
     }
